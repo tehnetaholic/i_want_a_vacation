2ce7c121db4d435778f22e0986b0644a25e8a392
Sergei Pankratov <teh.netaholic@gmail.com>
2023-06-01 16:25:50 +0400

    first commit

--- source/index.ts
+++ source/index.ts
@@ -1,7 +1,19 @@
 import { feedTheDragons } from './magic.js';
 import { start as startBot } from './bot/index.js';
+import express from 'express'
+
+const app = express()
 
 feedTheDragons();
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
 startBot();
+
+
+app.get('/', (_, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(process.env.PORT || 5000, () => {
+    console.log(`app is listening on port ${process.env.PORT || 5000}!`)
+});
